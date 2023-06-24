5212e5e0d72c0cd41c1a7e3fa86d6b6e278d1667
kencx <kennethcheo@pm.me>
2023-06-24 22:39:47 +0800

    Move GunicornApp into calibre_rest module

--- app.py
+++ app.py
@@ -2,52 +2,8 @@
 
 import argparse
 
-from gunicorn.app.base import BaseApplication
-
-from calibre_rest import create_app
+from calibre_rest import GunicornApp, create_app
 from config import ProdConfig
-
-
-class GunicornApp(BaseApplication):
-    defaults = {
-        "bind": "localhost:5000",
-        # calibredb supports only one operation at any single time. Increasing
-        # the number of workers would result in concurrent execution errors.
-        "workers": 1,
-        "backlog": 100,
-        "worker_class": "sync",
-        "timeout": 30,
-        "keepalive": 2,
-        "spew": False,
-        "daemon": False,
-        "raw_env": [],
-        "pidfile": "/tmp/gunicorn_vm_api.pid",
-        "umask": 755,
-        "user": 1000,
-        "group": 1000,
-        "tmp_upload_directory": None,
-        # Log errors to stdout
-        "error_log": "-",
-        "access_log": "-",
-    }
-
-    def __init__(self, app, **kwargs):
-        self.options = {**self.defaults, **kwargs}
-        self.app = app
-        super().__init__()
-
-    def load_config(self):
-        config = {
-            key: value
-            for key, value in self.options.items()
-            if key in self.cfg.settings and value is not None
-        }
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.app
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Start Gunicorn server")
--- calibre_rest/__init__.py
+++ calibre_rest/__init__.py
@@ -1,11 +1,53 @@
 import logging
 
 from flask import Flask
+from gunicorn.app.base import BaseApplication
 
 from calibre_rest.calibre import CalibreWrapper
 from config import config_map
 
 LOG_FORMAT = "%(asctime)s %(name)s - %(levelname)s - %(message)s"
+
+
+class GunicornApp(BaseApplication):
+    defaults = {
+        "bind": "localhost:5000",
+        # calibredb supports only one operation at any single time. Increasing
+        # the number of workers would result in concurrent execution errors.
+        "workers": 1,
+        "backlog": 100,
+        "worker_class": "sync",
+        "timeout": 30,
+        "keepalive": 2,
+        "spew": False,
+        "daemon": False,
+        "raw_env": [],
+        "pidfile": "/tmp/gunicorn_vm_api.pid",
+        "umask": 755,
+        "user": 1000,
+        "group": 1000,
+        "tmp_upload_directory": None,
+        # Log errors to stdout
+        "error_log": "-",
+        "access_log": "-",
+    }
+
+    def __init__(self, app, **kwargs):
+        self.options = {**self.defaults, **kwargs}
+        self.app = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.app
 
 
 def create_app(config_name="default"):
